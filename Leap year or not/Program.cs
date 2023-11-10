1ef7107a1eee3e532b4ad08fbd18b6bb2ec0c1d2
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-11-10 22:13:29 +0530

    leap year or not

---
+++ Leap year or not/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Enter your year :");
+int year=int.Parse(Console.ReadLine());
+
+if(year%400==0){
+    Console.WriteLine($"{year} = is leap year");
+}
+
+else if(year%4==0&& year%100!=0){
+   Console.WriteLine($"{year} = is leap year");
+}
+
+else{
+   Console.WriteLine($"{year} = is not leap year"); 
+}
