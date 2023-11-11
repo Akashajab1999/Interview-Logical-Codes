48c8e718ac2cb5abb019cd5588e74ed98f2735d2
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-11-11 21:49:44 +0530

    some of digits of given number

---
+++ sum of digits of number/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter your Number :");
+int number =int.Parse(Console.ReadLine());
+int reminder=0;
+int sum =0;
+while(number!=0){
+    reminder=number%10;
+    sum+=reminder;
+    number=number/10;
+}
+
+Console.WriteLine($"Sum of digits = {sum}");
