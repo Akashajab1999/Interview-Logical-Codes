8b6d3f38188ffbe9e92626e6723a7cef5c65da18
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-11-11 21:19:57 +0530

    prime number

---
+++ Prime Number/Program.cs
@@ -0,0 +1,27 @@
+﻿bool IsPrime(int number){
+    bool status=true;
+    if(number<2){
+        status=false;
+       return status; 
+    }
+
+    for(int i=2;i<number;i++){
+        if(number%i==0){
+            status=false;
+            return status;
+        }
+    }
+    return status;
+}
+
+ Console.WriteLine("Enter your Number :");
+ int number =int.Parse(Console.ReadLine());
+ if(IsPrime(number)){
+    Console.WriteLine("Given Number is Prime Number");
+ }
+
+ else{
+    Console.WriteLine("Not prime number");
+ }
+
+
