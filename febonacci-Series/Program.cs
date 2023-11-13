edde7c4772381010fd1c2045ce34b7b1c26482f8
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2023-11-13 19:50:23 +0530

    febonaci series

---
+++ febonacci-Series/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+int firstNumber=0;
+int secondNumber=1;
+int sum =0;
+int febonacciSeries(int level){
+for(int i=2;i<level;i++){
+
+   sum=firstNumber+secondNumber;
+   firstNumber=secondNumber;
+   secondNumber=sum;
+
+    
+}
+
+return sum;
+}
+
+ Console.WriteLine( febonacciSeries(15));
